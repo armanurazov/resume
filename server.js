2a85eb0848d85aef71bbe604289ccc51309e785b
Arman Urazov <aurazov@myseneca.ca>
2022-02-21 03:10:50 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const PORT = 8080;
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/about', (req, res) =>{
+    res.sendFile(__dirname + '/about.html');
+})
+
+app.get('/education', (req, res) =>{
+    res.sendFile(__dirname + '/education.html');
+})
+
+app.get('/work', (req, res) =>{
+    res.sendFile(__dirname + '/work.html');
+})
+
+app.get('/contact', (req, res) =>{
+    res.sendFile(__dirname + '/contact.html');
+})
+
+app.listen(PORT, console.log('Connected to ' + PORT));
